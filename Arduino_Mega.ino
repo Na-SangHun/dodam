f295c7a802fedb363350a731063ddf901e325618
Na-SangHun <nasangchu33@gmail.com>
2026-03-25 21:49:22 +0900

    Refactor comments and update gas threshold

--- Arduino_Mega.ino
+++ Arduino_Mega.ino
@@ -1,15 +1,15 @@
 //================================================================
 // IoT 캡스톤디자인: 비접촉식 고독사 방지 시스템
-// 역할: 아두이노 메가 - 환경 센서 데이터 수집 및 전송 허브 (가스 감지 알람 추가)
+// 아두이노 메가 - 환경 센서 데이터 수집 및 전송 허브
 //================================================================
 
-// --- 1. 라이브러리 포함 ---
+// --- 라이브러리 ---
 #include <DHT.h>      // 온습도 센서 라이브러리
 #include <Wire.h>     // I2C 통신 라이브러리   // 조도 센서 라이브러리
 #include <MQ135.h>    // 유해가스 센서 라이브러리
 #include <Servo.h> // 서브모터 센서 라이브러리
 
-// --- 2. 핀 번호 및 상수 정의 ---
+// --- 핀 번호 및 상수 정의 ---
 // 센서 핀
 #define DHT11_PIN      2      // DHT11 센서 DATA 핀
 #define MQ135_PIN      A12    // MQ135 센서 AOUT 핀
@@ -18,7 +18,7 @@
 const long BAUD_RATE = 9600;
 const int SENSOR_READ_PERIOD = 2000; // 2초 주기로 데이터 전송
 
-// --- 3. 센서 객체 및 전역 변수 선언 ---
+// --- 센서 객체 및 전역 변수 선언 ---
 // 센서 객체
 DHT dht11(DHT11_PIN, DHT11);
 MQ135 mq135(MQ135_PIN);
@@ -33,7 +33,7 @@ int GAS = 0;
 unsigned long t_now = 0;
 unsigned long t_prev_sensor = 0;
 
-// --- 4. 초기 설정 함수: setup() ---
+// --- 초기 설정 함수: setup() ---
 void setup() {
   Serial.begin(BAUD_RATE);      // PC와의 통신 시작 (디버깅용)
   Serial1.begin(BAUD_RATE);     // 라즈베리파이(HC-06)와의 통신 시작
@@ -47,13 +47,12 @@ void setup() {
   t_prev_sensor = millis();
 }
 
-// --- 5. 메인 루프 함수: loop() ---
+// --- 메인 루프 함수: loop() ---
 void loop() {
   t_now = millis();
 
-  // --- 2초 주기: 전체 센서 데이터 취합 및 전송 ---
+  // --- 2초 주기로 전체 센서 데이터 취합 및 전송 ---
   if (t_now - t_prev_sensor >= SENSOR_READ_PERIOD) {
-    // 아날로그 및 I2C 센서 값 읽기
     TEMP = dht11.readTemperature();
     RH = dht11.readHumidity();
 
@@ -64,8 +63,7 @@ void loop() {
   
     GAS = mq135.getCorrectedPPM(TEMP, RH);
 
-    // <<-- [추가된 부저 알람 로직] -->>
-    // 가스 수치가 3000을 초과하는지 확인
+    // 가스 수치가 1180을 초과하는지 확인
     if (GAS >= 1180) {
       myservo.write(160);
       delay(10);
@@ -76,7 +74,7 @@ void loop() {
     }
     // ------------------------------------
     
-    // 데이터 포장 (char 배열 사용)
+    // 데이터 포장
     char data_buffer[50]; 
     sprintf(data_buffer, "%d,%d,%d",
             TEMP, RH,  GAS);
